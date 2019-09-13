baccaa644661ed74929a84a56267a6f5abb9281e
Cris Mihalache <me@f3rno.com>
2019-09-14 01:28:04 +0700

    (refactor) use mocha for test

---
+++ test/accumulative_swing_index.js
@@ -0,0 +1,25 @@
+/* eslint-env mocha */
+'use strict'
+
+const assert = require('assert')
+const { ASI } = require('../dist')
+
+const candles = [{
+  open: 6373,
+  high: 6375.9,
+  low: 6369.2,
+  close: 6374.5
+}, {
+  open: 6408.8,
+  high: 6408.9,
+  low: 6366,
+  close: 6372.9
+}]
+
+describe('Accumulative Swing Index', () => {
+  it('is calculated properly', () => {
+    const asi = new ASI([6400])
+    candles.forEach(c => asi.add(c))
+    assert.strictEqual(asi.v().toFixed(13), '-0.1190821779318')
+  })
+})
