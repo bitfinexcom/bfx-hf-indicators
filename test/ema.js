d434ac49f0cc80e1f511c886b03cd34b029e7ed6
Abhishek Shrestha <avsek.stha07@gmail.com>
2021-05-06 14:46:47 +0545

    ema updated to return null if provided candles aren't enough

--- test/ema.js
+++ test/ema.js
@@ -4,57 +4,93 @@
 const assert = require('assert')
 const EMA = require('../lib/ema')
 
-const emaPeriod = [10]
-const candles = [{
-  open: 6373,
-  high: 6375.9,
-  low: 6369.2,
-  close: 6374.5
-}, {
-  open: 6408.8,
-  high: 6408.9,
-  low: 6366,
-  close: 6372.9
-}]
+const emaPeriod = [3]
+const candles = [
+  { open: 6373, high: 6375.9, low: 6369.2, close: 6374.5 },
+  { open: 6374.5, high: 6408.9, low: 6366, close: 6372.9 },
+  { open: 6372.9, high: 6373.9, low: 6360.1, close: 6370.9 },
+  { open: 6370.9, high: 6400, low: 6345, close: 6400 }
+]
 
 describe('Exponential Moving Average(EMA)', () => {
   it('sets the seed period properly', () => {
     const ema = new EMA(emaPeriod)
-    assert.deepStrictEqual(ema._seedPeriod, 10)
+    assert.deepStrictEqual(ema._seedPeriod, 3)
   })
 
   it('sets the weighted multiplier properly', () => {
     const ema = new EMA(emaPeriod)
-    assert.deepStrictEqual(ema._a, 0.18181818181818182)
+    assert.deepStrictEqual(ema._a, 0.5)
+  })
+
+  it('calculates ema properly when emaPeriod is 1', () => {
+    const ema = new EMA([1])
+    candles.forEach(c => ema.add(c.close))
+    assert.deepStrictEqual(ema.v(), 6400)
+    assert.deepStrictEqual(ema._buffer, [])
+    assert.deepStrictEqual(ema._values, candles.map(c => c.close))
+  })
+
+  it('updates ema value properly when emaPeriod is 1', () => {
+    const ema = new EMA([1])
+    candles.forEach(c => ema.add(c.close))
+    assert.deepStrictEqual(ema.v(), 6400)
+    ema.update(6500)
+    assert.deepStrictEqual(ema.v(), 6500)
   })
 
   it('calculates ema based on close price properly', () => {
     const ema = new EMA(emaPeriod)
     candles.forEach(c => ema.add(c.close))
-    assert.deepStrictEqual(ema.v(), 6374.209090909091)
+    assert.deepStrictEqual(ema.v(), 6386.15)
   })
 
-  it('calculates ema based on open price properly', () => {
+  it('empties the buffer and ema values on reset', () => {
     const ema = new EMA(emaPeriod)
-    candles.forEach(c => ema.add(c.open))
-    assert.deepStrictEqual(ema.v(), 6379.509090909091)
+    candles.forEach(c => ema.add(c.close))
+    assert.deepStrictEqual(ema.v(), 6386.15)
+    assert.deepStrictEqual(ema._buffer.length, 2)
+    assert.deepStrictEqual(ema._values.length, 2)
+    ema.reset()
+    assert.deepStrictEqual(ema._buffer.length, 0)
+    assert.deepStrictEqual(ema._values.length, 0)
   })
 
-  it('calculates ema based on high price properly', () => {
+  it('returns ema as null if the given time period is less than the provided candles', () => {
+    const ema = new EMA([candles.length + 1])
+    candles.forEach(c => ema.add(c.open))
+    assert.deepStrictEqual(ema.v(), null)
+  })
+
+  it('calculates ema properly when number of candles provided are same as the ema period', () => {
+    const ema = new EMA([candles.length])
+    candles.forEach(c => ema.add(c.high))
+    assert.deepStrictEqual(ema.v(), 6389.812)
+  })
+
+  it('calculates ema properly based on the updated value', () => {
     const ema = new EMA(emaPeriod)
     candles.forEach(c => ema.add(c.high))
-    assert.deepStrictEqual(ema.v(), 6381.9)
+    assert.deepStrictEqual(ema.v(), 6391.575)
+    ema.update(6450)
+    assert.deepStrictEqual(ema.v(), 6416.575)
   })
 
-  it('calculates ema based on low price properly', () => {
-    const ema = new EMA(emaPeriod)
-    candles.forEach(c => ema.add(c.low))
-    assert.deepStrictEqual(ema.v(), 6368.618181818182)
+  it('updates the buffer and returns null if the given time period is less than the provided candles', () => {
+    const ema = new EMA([2])
+    const candlePriceOpen = 6600
+    const updatedCandlePrice = 6601
+    ema.add(candlePriceOpen)
+    assert.deepStrictEqual(ema._buffer, [candlePriceOpen])
+    ema.update(updatedCandlePrice)
+    assert.deepStrictEqual(ema._buffer, [updatedCandlePrice])
+    assert.deepStrictEqual(ema.v(), null)
   })
 
-  it('does not use the multiplier for calculating ema for first candle', () => {
-    const ema = new EMA(emaPeriod)
-    ema.add(candles[0].close)
-    assert.deepStrictEqual(ema.v(), candles[0].close)
+  it('updates the ema value properly when number of candles provided are same as the ema period', () => {
+    const ema = new EMA([candles.length])
+    candles.forEach(c => ema.add(c.high))
+    ema.update(6420)
+    assert.deepStrictEqual(ema.v(), 6397.812)
   })
 })
